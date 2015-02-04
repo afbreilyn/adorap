cd51deb93051b743f302ae5d1a98632e0b892726
afbresley <afbresley@gmail.com>
2015-02-04 17:10:40 -0500

    Pasted schema onto model pages for clarities sake. Put more model attributes onto the index page.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,15 @@
+#  create_table "abilities", force: true do |t|
+#    t.string   "scales_from"
+#    t.string   "key"
+#    t.string   "name"
+#    t.text     "description"
+#    t.integer  "abilitable_id"
+#    t.string   "abilitable_type"
+#    t.datetime "created_at"
+#    t.datetime "updated_at"
+#  end
+
+
 class Ability < ActiveRecord::Base
   belongs_to :abilitable, polymorphic: true
 end
--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,7 +1,19 @@
+#  create_table "champions", force: true do |t|
+#    t.string   "name"
+#    t.text     "lore"
+#    t.datetime "created_at"
+#    t.datetime "updated_at"
+#    t.string   "loadingart_file_name"
+#    t.string   "loadingart_content_type"
+#    t.integer  "loadingart_file_size"
+#    t.datetime "loadingart_updated_at"
+#    t.string   "primary_damage_type"
+#  end
+
 class Champion < ActiveRecord::Base
   has_many :abilities, as: :abilitable
 
-  has_attached_file :loadingart, :styles => { :medium => "308x560>" } 
+  has_attached_file :loadingart, :styles => { :medium => "308x560>", :small => "154x280" } 
   validates_attachment_content_type :loadingart, :content_type => /\Aimage\/.*\Z/
 
 end
