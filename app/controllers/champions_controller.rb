44d8cff02c10945a028dfcfa6627a0f7edf52174
afbresley <afbresley@gmail.com>
2015-02-09 19:57:51 -0500

    Moved ajax/js to index.haml and added ajax call.

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -5,7 +5,15 @@ class ChampionsController < ApplicationController
 	end
 
   def show
-    @champion = Campion.find_by(:id)
+    champ_id = find_camp_with_js_id(params)
+    @champion = Champion.find_by_id(champ_id)
+
+    render 'champion_show'
   end
+
+  private
+    def find_camp_with_js_id(params)
+      params['js_id'].split('list')[1]
+    end
   
 end
